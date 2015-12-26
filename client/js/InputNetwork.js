38aedd2d8fb12cdba4c242f7d6886acb994c5060
Matthieu Falce <falce.matthieu@gmail.com>
2015-12-26 17:33:25 +0100

    adds networking code to communicate between the clients

---
+++ client/js/InputNetwork.js
@@ -0,0 +1,178 @@
+
+function InputNetwork () {
+  this.left = false;
+  this.leftTime = 0;
+  this.up = false;
+  this.upTime = 0;
+  this.right = false;
+  this.rightTime = 0;
+  this.down = false;
+  this.downTime = 0;
+
+  this.active = true;
+}
+
+
+InputNetwork.prototype = {
+  initNetwork: function() {
+    this.peer = new Peer({key: '0opo1rm535xc4n29'});
+
+    this.peer.on('connection', function(c){
+      console.log("connection request from : "+ c.peer);
+      this.connect(c)
+    });
+
+    this.peer.on('error', function(err) {
+      console.log(err);
+    })
+  },
+
+
+  connect: function (c) {
+    var that = this;
+    c.on('data', function(data) {
+      console.log(data)
+      if (data.down){
+        switch (parseInt(data.down)) {
+          case 37:
+            if (!that.left)
+              that.leftTime = Date.now();
+            that.left = true;
+            break;
+          case 38:
+            if (!that.up)
+              that.upTime = Date.now();
+            that.up = true;
+            break;
+          case 39:
+            if (!that.right)
+              that.rightTime = Date.now();
+            that.right = true;
+            break;
+          case 40:
+            if (!that.down)
+              that.downTime = Date.now();
+            that.down = true;
+            break;
+          default:
+        }
+      }
+      else if (data.up){
+        switch (parseInt(data.up)) {
+          case 37:
+            that.left = false;
+            break;
+          case 38:
+            that.up = false;
+            break;
+          case 39:
+            that.right = false;
+            break;
+          case 40:
+            that.down = false;
+            break;
+          default:
+      }
+
+      }
+    });
+    c.on('close', function() {
+      alert(c.peer + ' has left the chat.');
+    });
+  },
+
+  connect_to_id: function(id_) {
+    var that = this;
+    var c = this.peer.connect(id_);
+    c.on('open', function() {
+      that.connect(c);
+    });
+    c.on('error', function(err) {
+      alert(err);
+    });
+    console.log("triyng to connect to : "+id_);
+  },
+
+  send_message: function(msg) {
+    for (other_id in this.peer.connections) {
+      for(var i = 0; i < this.peer.connections[other_id].length; i+=1){
+        var c = this.peer.connections[other_id][i];
+        c.send(msg);
+        console.log("sent message : " + msg + " to : " + c.peer);
+      };
+    }
+  },
+
+  gameControl: function () {
+    var that = this;
+    window.onkeydown = function (event) {
+      that.send_message({"down":event.keyCode})
+      switch (event.keyCode) {
+        case 37:
+          if (!that.left)
+            that.leftTime = Date.now();
+          that.left = true;
+          break;
+        case 38:
+          if (!that.up)
+            that.upTime = Date.now();
+          that.up = true;
+          break;
+        case 39:
+          if (!that.right)
+            that.rightTime = Date.now();
+          that.right = true;
+          break;
+        case 40:
+          if (!that.down)
+            that.downTime = Date.now();
+          that.down = true;
+          break;
+        default:
+      }
+    }
+
+    window.onkeyup = function (event) {
+      that.send_message({"up":event.keyCode})
+      switch (event.keyCode) {
+        case 37:
+          that.left = false;
+          break;
+        case 38:
+          that.up = false;
+          break;
+        case 39:
+          that.right = false;
+          break;
+        case 40:
+          that.down = false;
+          break;
+        default:
+      }
+    }
+  },
+
+  startScreenControl: function () {
+    var that = this;
+
+    window.onkeydown = function (event) {
+      var key = event.keyCode;
+
+      if (key >= 37 && key <= 40) {
+        that.gameControl();
+        that.active = true;
+
+        var event = new Event('keydown');
+        event.keyCode = key;
+        window.dispatchEvent(event);
+
+        var event = new Event('levelStart');
+        window.dispatchEvent(event);
+      }
+    }
+  }
+};
+
+var inputKeyboard = new InputNetwork();
+inputKeyboard.startScreenControl ();
+inputKeyboard.initNetwork();
