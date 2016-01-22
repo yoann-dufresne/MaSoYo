9251d289a0ab6fe173b0c2442a75735c8462af6b
Matthieu Falce <falce.matthieu@gmail.com>
2016-01-22 16:05:53 +0100

    fixes the peerjs server and adds a URI to get the list of connected hosts

--- server/main.js
+++ server/main.js
@@ -1,39 +1,47 @@
-// var express = require('express');
-// var app = express();
-// var ExpressPeerServer = require('peer').ExpressPeerServer;
-// 
-// app.get('/', function(req, res, next) { res.send('Hello world!'); });
-// 
-// var server = app.listen(9000);
-// 
-// var options = {
-//     debug: true
-// }
-// 
-// app.use('/api', ExpressPeerServer(server, options));
-// 
-// server.on('connection', function(id) {
-// 	 console.log(id) 
-// 	
-// });
-// server.on('disconnect', function(id) { console.log(id + "deconnected") });
-// 
+var express = require('express');
+var app = express();
+var ExpressPeerServer = require('peer').ExpressPeerServer;
 
-
-var ip = require('ip');
-var PeerServer = require('peer').PeerServer;
-
-var port = 9000;
-var server = new PeerServer({port: port, allow_discovery: true});
-
-server.on('connection', function (id) {
-  console.log('new connection with id ' + id);
+app.get('/', function(req, res, next) {
+    console.log("get /")
+    res.send('Hello world!');
 });
 
-server.on('disconnect', function (id) {
-  console.log('disconnect with id ' + id);
+var server = app.listen(9000);
+
+var options = {
+    debug: true,
+    allow_discovery: true
+}
+
+var connected = [];
+
+peerServer = ExpressPeerServer(server, options)
+app.use('/api', peerServer);
+
+peerServer.on('connection', function(id) {
+  console.log("connection from: " + id)
+  var idx = connected.indexOf(id); // only add id if it's not in the list yet
+  if (idx === -1) {
+    connected.push(id);
+  }
 });
 
-console.log('peer server running on ' +
-            ip.address() + ':' + port);
+peerServer.on('disconnect', function(id) {
+  console.log("deconnection from:" + id)
+  var idx = connected.indexOf(id); // only attempt to remove id if it's in the list
+  if (idx !== -1) {
+    connected.splice(idx, 1);
+  }
+});
 
+app.get('/connected-people', function (req, res) {
+
+  // allow cross domain
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+
+
+  return res.json(connected);
+});
