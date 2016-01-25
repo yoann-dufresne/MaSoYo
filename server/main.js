6c7c60b98200c969215717c21d399b2d5cd640e2
Yoann Dufresne <you@example.com>
2016-01-25 19:44:32 +0100

    changes server to autoconnect peers

--- server/main.js
+++ server/main.js
@@ -2,11 +2,6 @@ var express = require('express');
 var app = express();
 var ExpressPeerServer = require('peer').ExpressPeerServer;
 
-app.get('/', function(req, res, next) {
-  console.log("get /")
-  res.send('Hello world!');
-});
-
 var port = 1414;
 var server = app.listen(port);
 
@@ -47,4 +42,39 @@ app.get('/connected-people', function (req, res) {
 
 
   return res.json(connected);
+});
+
+app.get('/', function(req, res, next) {
+  console.log("get /")
+  res.send('Hello world!');
+});
+
+
+var select_other = function(others) {
+  var other = others[Math.floor(Math.random()*others.length)];
+  console.log("chosen one : ", other);
+  if (typeof other === 'undefined') {
+    return {'error': 'not found', 'data': []}
+  }
+  else {
+    return {'error': '', 'data': other}
+  }
+}
+
+app.get('/connect/:me', function (req, res) {
+
+  // allow cross domain
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+
+  var others = JSON.parse(JSON.stringify(connected));
+  console.log("connected people : ", others)
+  var idx = connected.indexOf(req.params.me); // only attempt to remove id if it's in the list
+  console.log("me : ", req.params.me)
+  if (idx !== -1) {
+    others.splice(idx, 1);
+  }
+
+  return res.json(select_other(others));
 });
